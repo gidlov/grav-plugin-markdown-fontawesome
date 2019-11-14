c3cffdb863a067b6aaaf6379de5a1ef22e068fd3
gidlov <klas.gidlov@gmail.com>
2019-11-14 22:02:08 +0100

    Option to use Line Awesome

--- markdown-fontawesome.php
+++ markdown-fontawesome.php
@@ -6,6 +6,10 @@ use RocketTheme\Toolbox\Event\Event;
 
 class MarkdownFontAwesomePlugin extends Plugin
 {
+
+    const PLUGIN_CONFIG_PATH = 'plugins.markdown-fontawesome';
+
+
     /**
      * @return array
      */
@@ -23,10 +27,19 @@ class MarkdownFontAwesomePlugin extends Plugin
         // Initialize Text example
         $markdown->addInlineType(':', 'FontAwesome');
 
+        // Font Awesome or Line Awesome
+        $fala = $this->_getConfigSetting('fala');
+
+        if ($fala === 0) {
+            $class = "la";
+        } else {
+            $class = "fa";
+        }
+
         // Add function to handle this
-        $markdown->inlineFontAwesome = function($excerpt) {
+        $markdown->inlineFontAwesome = function($excerpt) use ($class) {
             // Search $excerpt['text'] for regex and store whole matching string in $matches[0], store icon name in $matches[1]
-            if (preg_match('/:fa-([-\w]+):/', $excerpt['text'], $matches))
+            if (preg_match('/^:'.$class.'-([a-zA-Z0-9- ]+):/', $excerpt['text'], $matches))
             {
                 return array(
                     'extent' => strlen($matches[0]),
@@ -34,11 +47,34 @@ class MarkdownFontAwesomePlugin extends Plugin
                         'name' => 'i',
                         'text' => '',
                         'attributes' => array(
-                            'class' => 'fa fa-'.$matches[1],
+                            'class' => $class.' '.$class.'s '.$class.'-'.$matches[1],
+                        ),
+                    ),
+                );
+            }
+            elseif (preg_match('/^:'.$class.'([srldb]?) '.$class.'-([a-zA-Z0-9- ]+):/', $excerpt['text'], $matches))
+            {
+                return array(
+                    'extent' => strlen($matches[0]),
+                    'element' => array(
+                        'name' => 'i',
+                        'text' => '',
+                        'attributes' => array(
+                            'class' => $class.$matches[1].' '.$class.'-'.$matches[2],
                         ),
                     ),
                 );
             }
         };
+    }
+
+    /**
+     * Gets values for a specific config node of the plugin
+     *
+     * @param string $node
+     * @return mixed
+     */
+    protected function _getConfigSetting($node) {
+        return $this->config->get(self::PLUGIN_CONFIG_PATH . '.' . $node);
     }
 }
